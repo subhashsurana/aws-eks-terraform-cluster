ae47f3e13eb136e22e4ff453d0e46f9e000d5e8d
Subhash Surana <subhash.surana23@gmail.com>
2022-01-31 15:37:19 +0530

    Changed userdata bootstrap script to enable deployment of Nodegroups

--- modules/eks-workers/user-data.tf
+++ modules/eks-workers/user-data.tf
@@ -8,13 +8,7 @@ Content-Type: text/x-shellscript; charset="us-ascii"
 
 #!/bin/bash -xe
 set -o xtrace
-sudo /etc/eks/bootstrap.sh --apiserver-endpoint '${var.cluster_endpoint}' --b64-cluster-ca '${var.cluster_cert_authority}' 
-'${var.cluster_full_name}'
-echo "Running custom user data script" > /tmp/me.txt
-yum install -y amazon-ssm-agent
-echo "yum'd agent" >> /tmp/me.txt
-systemctl enable amazon-ssm-agent && systemctl start amazon-ssm-agent
-date >> /tmp/me.txt
+/etc/eks/bootstrap.sh --apiserver-endpoint '${var.cluster_endpoint}' --b64-cluster-ca '${var.cluster_cert_authority}' '${var.cluster_full_name}'
 
 --==MYBOUNDARY==--
 USERDATA
